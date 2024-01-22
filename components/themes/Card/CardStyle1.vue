806a22f52199fc4d8f895e7304a40f6fecb932e5
Gevorg Daghlaryan <gevorg.daghlaryan@esterox.am>
2024-01-22 11:49:29 +0400

    fix cards style

--- components/themes/Card/CardStyle1.vue
+++ components/themes/Card/CardStyle1.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="card-wrapper">
-    <div class="service-box d-inherit p-4 my-3 wow fadeInUp text-left">
+    <div class="service-box  p-4 my-3 wow fadeInUp text-left d-flex flex-column justify-content-between">
       <slot name="cardMedia" />
-      <div class="service-detail align-self-center">
+      <div class="service-detail ">
         <slot name="cardTitle" />
         <slot name="cardBody" />
         <slot name="cardList" />
         <slot name="cardInfo" />
-        <slot name="cardLink" />
       </div>
+      <slot name="cardLink" />
     </div>
   </div>
 </template>
